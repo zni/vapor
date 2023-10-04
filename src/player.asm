f36eed14b9145e2c88dd50e8b12042f2f02cf25e
zni <>
2023-10-04 18:42:31 -0400

    It moves. And so, there was hope.

--- src/player.asm
+++ src/player.asm
@@ -42,41 +42,45 @@
     TYA
     PHA
 
-    LDA player_x
-    CMP #$e0
-    BCC not_at_right_edge
-    ; if not BCC, we are greater than $e0
-    LDA #$00
-    STA player_dir          ; start moving left
-    JMP direction_set
-
-not_at_right_edge:
-    LDA player_x
-    CMP #$10
-    BCS direction_set
-    ; if not BCS, we are less than $10
+;;;; get controller input
     LDA #$01
-    STA player_dir
+    STA CONTROLLER_1
+    LDA #$00
+    STA CONTROLLER_1
 
-direction_set:
-    LDA player_dir
-    CMP #$01
-    BEQ move_right
-    ; if player_dir - $01 is not zero,
-    ; that means player_dir was $00 and
-    ; we need to move left
-    SEC
-    LDA player_x
-    SBC #PLAYER_SPEED
-    STA player_x
-    JMP exit_subroutine
-move_right:
-    CLC
-    LDA player_x
-    ADC #PLAYER_SPEED
-    STA player_x
+    ; initialize pad1
+    LDA #%00000001
+    STA pad1
 
-exit_subroutine:
+get_button_states:
+    LDA CONTROLLER_1
+    LSR A            ; Shift the accumulator into the carry flag.
+    ROL pad1         ; Shift everything in pad1,
+                     ; bringing the carry flag into pad1.
+    BCC get_button_states
+;;;; end get input
+
+;;;; check button presses
+    LDA pad1
+    AND #BTN_LEFT
+    BEQ check_right
+    DEC player_x
+check_right:
+    LDA pad1
+    AND #BTN_RIGHT
+    BEQ check_up
+    INC player_x
+check_up:
+    LDA pad1
+    AND #BTN_UP
+    BEQ check_down
+    DEC player_y
+check_down:
+    LDA pad1
+    AND #BTN_DOWN
+    BEQ done_checking
+    INC player_y
+done_checking:
     PLA
     TAY
     PLA
@@ -87,4 +91,5 @@ exit_subroutine:
 .endproc
 
 .segment "ZEROPAGE"
+pad1: .res 1
 .importzp player_x, player_y, player_dir
